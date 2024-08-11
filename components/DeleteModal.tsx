0130c1776fa12944246e2ee7c2a19b24ab043ac9
AvikNayak <nayakavik19@gmail.com>
2024-08-11 18:55:06 +0530

    Delete modal functionality added

---
+++ components/DeleteModal.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { db, storage } from "@/firebase";
+import { useAppStore } from "@/store/store";
+import { useUser } from "@clerk/nextjs";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+
+export function DeleteModal() {
+  const { user } = useUser();
+
+  const [fileId, setFileId, isDeleteModalOpen, setIsDeleteModalOpen] =
+    useAppStore((state) => [
+      state.fileId,
+      state.setFileId,
+      state.isDeleteModalOpen,
+      state.setIsDeleteModalOpen,
+    ]);
+
+  async function deleteFile() {
+    if (!user || !fileId) return;
+
+    const fileRef = ref(storage, `users/${user.id}/files/${fileId}`);
+
+    try {
+      deleteObject(fileRef)
+        .then(async () => {
+          deleteDoc(doc(db, "users", user.id, "files", fileId)).then(() => {
+            console.log("File with id: " + fileId + " deleted");
+          });
+        })
+        .finally(() => {
+          setIsDeleteModalOpen(false);
+        });
+    } catch (error) {
+      console.log(error);
+      setIsDeleteModalOpen(false);
+    }
+  }
+
+  return (
+    <Dialog
+      open={isDeleteModalOpen}
+      onOpenChange={(isOpen) => {
+        setIsDeleteModalOpen(isOpen);
+      }}
+    >
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Are you sure you want to delete?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            file!
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter className="flex space-x-2 py-3">
+          <Button
+            size="sm"
+            className="px-3 flex-1"
+            variant="secondary"
+            onClick={() => setIsDeleteModalOpen(false)}
+          >
+            <span className="sr-only">Cancel</span>
+            <span>Cancel</span>
+          </Button>
+
+          <Button
+            type="submit"
+            size="sm"
+            className="px-3 flex-1"
+            variant="destructive"
+            onClick={() => deleteFile()}
+          >
+            <span className="sr-only">Delete</span>
+            <span>Delete</span>
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- components/table/data-table.tsx
+++ components/table/data-table.tsx
@@ -19,6 +19,7 @@ import { Button } from "../ui/button";
 import { FileType } from "@/typings";
 import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
 import { useAppStore } from "@/store/store";
+import { DeleteModal } from "../DeleteModal";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -82,6 +83,8 @@ export function DataTable<TData, TValue>({
                 key={row.id}
                 data-state={row.getIsSelected() && "selected"}
               >
+                <DeleteModal />
+
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
                     {cell.column.id === "timeStamp" ? (
