6be2eb1bcc9859791b1789eba85f79b0a55bed9a
AvikNayak <nayakavik19@gmail.com>
2024-08-13 11:46:23 +0530

    hidden firebase credentials

--- firebase.ts
+++ firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCFEcgG6eqi36sTAgAoJKEuZ9hipifKsLw",
-  authDomain: "boxdrive-a101b.firebaseapp.com",
-  projectId: "boxdrive-a101b",
-  storageBucket: "boxdrive-a101b.appspot.com",
-  messagingSenderId: "494633470698",
-  appId: "1:494633470698:web:83fbdfa84dcedc3d506593",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
